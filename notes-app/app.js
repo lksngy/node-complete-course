33f6f7f7470a9d65d8570b22f3325468630db2f0
lksngy <luk.nagy@gmail.com>
2021-02-27 00:02:26 +0100

    finished section 3

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,12 @@
+const chalk = require('chalk')
+const validator = require('validator')
 const getNotes = require('./notes.js')
 
 const yourNotes = getNotes();
+
+console.log('checking the email... ' + validator.isEmail('george@gmail.com'))
+
+console.log('Checking the mobile phone... ' + validator.isMobilePhone('+420739017529', 'de-DE', true))
+
+console.log(chalk.red('Printed in green!'))
+console.log(chalk.green.inverse('Printed in green, but inversed!'))
