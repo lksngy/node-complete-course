00fca75117ed1d5a0f354f166f1770e62b00660a
lksngy <luk.nagy@gmail.com>
2021-02-27 18:04:10 +0100

    working on notes app

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,5 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const validator = require('validator')
 const getNotes = require('./notes.js')
 
@@ -6,7 +7,33 @@ const yourNotes = getNotes();
 
 console.log('checking the email... ' + validator.isEmail('george@gmail.com'))
 
-console.log('Checking the mobile phone... ' + validator.isMobilePhone('+420739017529', 'de-DE', true))
-
-console.log(chalk.red('Printed in green!'))
 console.log(chalk.green.inverse('Printed in green, but inversed!'))
+
+
+// create read command
+yargs.command({
+    command: 'add',
+    describe: 'Add command',
+    builder: {
+        title: {
+            describe: 'Set title to your note',
+            demandOption: true,
+            type: 'string'
+
+        }
+    },
+    handler: function (argv) {
+        console.log('Adding your note', argv)
+    }
+})
+
+// create list command
+yargs.command({
+    command: 'list',
+    describe: 'List command',
+    handler: function () {
+        console.log('Listing your notes')
+    }
+})
+
+yargs.parse()
