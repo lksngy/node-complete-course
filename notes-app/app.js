8799efa612f012e52534c75f02456ab9410d1b94
lksngy <luk.nagy@gmail.com>
2021-03-07 22:55:03 +0100

    finished this section - add, list, remove, read notes functionality added

--- notes-app/app.js
+++ notes-app/app.js
@@ -47,7 +47,7 @@ yargs.command({
     command: 'list',
     describe: 'List all notes',
     handler () {
-        console.log('Listing your notes' + argv.title)
+        notes.listNotes()
     }
 })
 
@@ -55,8 +55,15 @@ yargs.command({
 yargs.command({
     command: 'read',
     describe: 'Reading all notes',
-    handler () {
-        console.log('reading your notes')
+    builder: {
+        title: {
+            describe: 'Read a note user choose',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler (argv) {
+       notes.readNote(argv.title)
     }
 })
 
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -13,23 +13,21 @@ const removeNote = (title) => {
     const notes = loadNotes()
     
     // filter out those with the same title as user requested, save the rest
-    const nonDuplicateNotes = notes.filter((note) => note.title !== title)
+    const nonDuplicateNote = notes.find((note) => note.title === title)
 
-    if (nonDuplicateNotes.length !== notes.length) {
+    if (!nonDuplicateNote) {
         console.log(chalk.green.inverse('This title: ' + title + ' was removed from the json file.'))
         // save newly created array
         saveNotes(nonDuplicateNotes)
     } else {
         console.log(chalk.red.inverse('No such title found, nothing deleted'))
     }
-    
-
 }
 
 // function to save data into json
 const addNote = (title, body) => {
     const notes = loadNotes()
-    console.log(notes)
+    //console.log(notes)
 
     // before we push data into notes, make sure to check for duplicates
     const duplicateNotes = notes.filter((note) => note.title === title)
@@ -44,10 +42,30 @@ const addNote = (title, body) => {
     saveNotes(notes)
     } else {
         console.log('Title already taken') // if there are duplicates, it will not put there
+    }    
+}
+
+//function to list all your notes
+const listNotes = () => {
+    const notes = loadNotes()
+    console.log(chalk.inverse.red('Your notes: '))
+    notes.forEach((el) => {
+        console.log(el.title)
+    })
+}
+
+// function to read chosen note
+const readNote = (title) => {
+    const notes = loadNotes()
+
+    const readThisOne = notes.find((note) => note.title === title)
+
+    if (readThisOne) {
+        console.log(chalk.inverse.blue(readThisOne.title))
+        console.log(readThisOne.body)
+    } else {
+        console.log(chalk.red.inverse('No such title found!'))
     }
-
-
-    
 }
 
 //little helper => save data into the file
@@ -70,5 +88,7 @@ const loadNotes = () => {
 module.exports = {
     getNotes: getNotes,
     addNote: addNote,
-    removeNote:removeNote
+    removeNote:removeNote,
+    listNotes: listNotes,
+    readNote: readNote
 }
